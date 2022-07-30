10ce5fd784ece09ce6a1d4a6beb5bd338a6e727e
litong1234567 <2724904118@qq.com>
2022-07-30 15:24:54 +0800

    IDEA_master2冲突合并

--- src/main/java/com/nsu/cn/test/test1.java
+++ src/main/java/com/nsu/cn/test/test1.java
@@ -12,5 +12,6 @@ public class test1 {
         System.out.println("1111111111111111");
         System.out.println("333333333333333");
         System.out.println("4444444444444");
+        System.out.println("IDEA_master2冲突合并");
     }
 }
